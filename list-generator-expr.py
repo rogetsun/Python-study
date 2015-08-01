6f4fbeb26f69b9d5a391c1858c1bef13dd466564
rogetsun <songyw163@163.com>
2015-08-01 20:33:03 +0800

    init

---
+++ list-generator-expr.py
@@ -0,0 +1,12 @@
+__author__ = 'uv2sun'
+
+
+L = [
+    i * 100 + j * 10 + n
+    for i in range(1, 10)
+    for j in range(0, 10)
+    for n in range(1, 10)
+    if i == n and i != j
+    ]
+
+print(L)
