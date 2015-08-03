4379b7b63780d5a9d2d1a522c30f83351a6d2892
rogetsun <songyw163@163.com>
2015-08-03 16:29:33 +0800

    init

--- filter.py
+++ filter.py
@@ -1,10 +1,13 @@
+# coding:utf-8
 __author__ = 'uv2sun'
 
+
 '''
-    注意filter返回的是一个iterator
+注意:
+    3.*版本中filter返回的是一个iterator
+    2.*版本中返回list
 '''
 l = filter(lambda x: x % 2 == 1, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(l)
 for i in l:
     print(i)
-
-
