6627f7b3bd746aed4006b40c8ecf42bf84ff0ad9
rogetsun <songyw163@163.com>
2015-08-03 17:28:06 +0800

    init

--- list-generator-expr.py
+++ list-generator-expr.py
@@ -9,3 +9,12 @@ L = [
     ]
 
 print(L)
+
+ll = []
+for i in range(1, 10):
+    for j in range(0, 10):
+        for n in range(1, 10):
+            if i == n and i != j:
+                ll.append(i * 100 + j * 10 + n)
+
+print(ll)
