8a5c4a71b4be6be3902768568e150d62a8d22e9d
rogetsun <songyw163@163.com>
2015-08-13 22:30:48 +0800

    init

--- time-test.py
+++ time-test.py
@@ -1,8 +1,25 @@
 __author__ = 'uv2sun'
 
 import time
+import datetime
 
-t = time.time()
+t = time.time() * 1000  # 毫秒级别
 print(t)
 timestamp = int(t)
 print(timestamp)
+
+print(1024 * 8 / 7)
+
+import json
+
+j = json.loads('{"a":1, "b":2}')
+print(j)
+print('len=%s' % len(j))
+print('type=%s' % type(j))
+print(j['a'])
+
+s = json.dumps(j)
+print(s)
+
+print(type(s))
+print(len(s))
